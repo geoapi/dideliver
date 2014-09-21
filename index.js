5527e0bd0b020535e78bf211796e92ba3522b20f
Geoapi <geo@itnet.uobabylon.edu.iq>
2014-09-22 07:37:18 +1000

    adding new sample

--- index.js
+++ index.js
@@ -1,39 +1,60 @@
-var fortune = require('fortune')
-  , express = fortune.express;
-
-/**
- * Example demonstrating two different databases being
- * exposed through one API. Note that each instance is
- * not automatically aware of any other instances, so
- * you will have to solve any problems that arise from
- * that yourself.
- */
-var container = express()
-  , port = process.argv[2] || 1337;
-
-var peopleAPI = fortune({
-  db: 'people'
-})
-.resource('person', {
-  name: String,
-  age: Number
+var fortune = require('fortune') 
+var app = fortune({
+db: "./db/restaurant",
+baseUrl: "gentle-forest-1449.herokuapp.com"
 });
 
-var animalsAPI = fortune({
-  db: 'animals'
-})
-.resource('animal', {
-  name: String,
-  type: String,
-  age: Number
+
+app.resource('restaurant',{
+	username:String,
+	password:String, //will check with stormpath and how we can represent it
+	title:String,
+	contact_person:String,
+	email:String,
+	phone:String,
+	restaurant_type:String,
+	address:String,
+	menu_r:['menu'],
+	r_has_rType:['restaurant_has_restaurantType']
 });
 
-container
-  .use(peopleAPI.router)
-  .use(animalsAPI.router)
-  .get('/', function(req, res) {
-    res.send('Hello, you have reached the zoo API.');
-  })
-  .listen(port);
+//app.resource('restaurant_has_restaurantType',{
+//	Restaurant_id:Number,
+//	RestaurantType_id:Number
+//});
 
-console.log('Listening on port ' + port + '...');
+
+app.resource('RestaurantType',{
+	name:String,
+	r_has_rType:['restaurant_has_restaurantType']
+});
+
+
+
+app.resource('menu',{
+	title:String,
+	price:Number,
+	image:String,
+	Description:String,
+	order1:['order']
+});
+app.resource('user', {
+	username: String,
+	firstname: String,
+	lastname: String,
+	email: String,
+	university: String,
+	order1:['order']
+});
+app.resource('order', {
+	delivery_address: String,
+	delivery_cost: Number,
+	cost:Number,
+	paid:Number,
+	created:String,
+	status_code:String
+});
+//app.get('/', function(request, response) {
+ // response.send('Hello World!')
+//});
+app.listen(process.argv[2] || 5000);
