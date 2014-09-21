71252a9a947c9776d8a77ad620efc7b0825c96f3
Geoapi <geo@itnet.uobabylon.edu.iq>
2014-09-22 07:22:36 +1000

    adding new sample

---
+++ back.js
@@ -0,0 +1,60 @@
+var fortune = require('fortune') 
+var app = fortune({
+db: "./db/restaurant",
+baseUrl: "gentle-forest-1449.herokuapp.com"
+});
+
+
+app.resource('restaurant',{
+	username:String,
+	password:String, //will check with stormpath and how we can represent it
+	title:String,
+	contact_person:String,
+	email:String,
+	phone:String,
+	restaurant_type:String,
+	address:String,
+	menu_r:['menu'],
+	r_has_rType:['restaurant_has_restaurantType']
+});
+
+//app.resource('restaurant_has_restaurantType',{
+//	Restaurant_id:Number,
+//	RestaurantType_id:Number
+//});
+
+
+app.resource('RestaurantType',{
+	name:String,
+	r_has_rType:['restaurant_has_restaurantType']
+});
+
+
+
+app.resource('menu',{
+	title:String,
+	price:Number,
+	image:String,
+	Description:String,
+	order1:['order']
+});
+app.resource('user', {
+	username: String,
+	firstname: String,
+	lastname: String,
+	email: String,
+	university: String,
+	order1:['order']
+});
+app.resource('order', {
+	delivery_address: String,
+	delivery_cost: Number,
+	cost:Number,
+	paid:Number,
+	created:String,
+	status_code:String
+});
+app.get('/', function(request, response) {
+  response.send('Hello World!')
+});
+app.listen(app.get('port'));
--- index.js
+++ index.js
@@ -1,60 +1,39 @@
-var fortune = require('fortune') 
-var app = fortune({
-db: "./db/restaurant",
-baseUrl: "gentle-forest-1449.herokuapp.com"
+var fortune = require('fortune')
+  , express = fortune.express;
+
+/**
+ * Example demonstrating two different databases being
+ * exposed through one API. Note that each instance is
+ * not automatically aware of any other instances, so
+ * you will have to solve any problems that arise from
+ * that yourself.
+ */
+var container = express()
+  , port = process.argv[2] || 1337;
+
+var peopleAPI = fortune({
+  db: 'people'
+})
+.resource('person', {
+  name: String,
+  age: Number
 });
 
-
-app.resource('restaurant',{
-	username:String,
-	password:String, //will check with stormpath and how we can represent it
-	title:String,
-	contact_person:String,
-	email:String,
-	phone:String,
-	restaurant_type:String,
-	address:String,
-	menu_r:['menu'],
-	r_has_rType:['restaurant_has_restaurantType']
+var animalsAPI = fortune({
+  db: 'animals'
+})
+.resource('animal', {
+  name: String,
+  type: String,
+  age: Number
 });
 
-//app.resource('restaurant_has_restaurantType',{
-//	Restaurant_id:Number,
-//	RestaurantType_id:Number
-//});
+container
+  .use(peopleAPI.router)
+  .use(animalsAPI.router)
+  .get('/', function(req, res) {
+    res.send('Hello, you have reached the zoo API.');
+  })
+  .listen(port);
 
-
-app.resource('RestaurantType',{
-	name:String,
-	r_has_rType:['restaurant_has_restaurantType']
-});
-
-
-
-app.resource('menu',{
-	title:String,
-	price:Number,
-	image:String,
-	Description:String,
-	order1:['order']
-});
-app.resource('user', {
-	username: String,
-	firstname: String,
-	lastname: String,
-	email: String,
-	university: String,
-	order1:['order']
-});
-app.resource('order', {
-	delivery_address: String,
-	delivery_cost: Number,
-	cost:Number,
-	paid:Number,
-	created:String,
-	status_code:String
-});
-app.get('/', function(request, response) {
-  response.send('Hello World!')
-});
-app.listen(app.get('port'));
+console.log('Listening on port ' + port + '...');
