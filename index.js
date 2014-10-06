6051555e42be5ed841c208d411e1a4098cd518b3
Simon Moller <Simon@Simons-MacBook-Air.local>
2014-10-06 22:57:37 +1100

    Headers

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var ddAPI = db.api;
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', req.headers.host);
 
     // Request methods you wish to allow
     //res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
