fbb0df5e4bf5df1b89cd7e352ab945488944665b
Simon Moller <Simon@Simons-MacBook-Air.local>
2014-10-08 20:41:41 +1100

    Removed some functions

--- index.js
+++ index.js
@@ -7,38 +7,7 @@ var db = require('./db'),
     rsvp = db.rsvp,
     ddAPI = db.api;
 
-/*
-var findMenuItems = function (ids, query, callback) {
 
-    console.log('function called');
-    var nameQuery = {'name': new RegExp(query, "i"), 'id': {$in : ids}};
-    var descQuery = {'description': new RegExp(query, "i"), 'id': {$in : ids}};
-    
-    var promises = {
-        name: ddAPI.adapter.find('menuItems', nameQuery),
-        description: ddAPI.adapter.find('menuItems', descQuery)
-    };
-    
-    console.log('Makes it above promises');
-    rsvp.hash(promises).then(function(results) {
-        console.log('Makes it to promises');
-        var nameItems = results.name.menuItems;
-        var descItems = results.description.menuItems;
-        var items = {};
-
-        nameItems.forEach(function(item) {
-            items[item.id] = item;
-        });
-
-        descItems.forEach(function(item) {
-            items[item.id] = item;
-        });
-
-        callback(items);
-    });
-    
-};
-*/
 //Allow cors 
 app.use(cors());
 
@@ -67,49 +36,6 @@ app.get('/uni_menu/:id', function(req, res) {
         });
     });
 });
-
-//Query to search menu items
-/*
-app.get('/search/menuItems', function(req,res) {
-    console.log('hits query');
-    //If restaurant parameter
-    if (req.query.restaurants) {
-        ddAPI.adapter.find('restaurant', req.query.restaurants).then(function(restaurant) {
-            findMenuItems(restaurant.restaurants[0].links.menuItems, req.query.search, function(items) {
-                res.json(items);
-            });
-        });
-    //If restaurant type parameter
-    } else if (req.query.types) {
-        ddAPI.adapter.find('restaurantType', req.query.types).then(function(types) {
-            ids = [];
-            types.restaurantTypes.forEach(function(type) {
-                ids.concat(type.links.restaurants);
-            });
-            ddAPI.adapter.findMany('restaurant', ids).then(function(restaurants) {
-                menuIds = [];
-                restaurants.restaurants.forEach(function(rest) {
-                    menuIds.concat(rest.links.menuItems);
-                });
-                findMenuItems(ids, req.query.search, function(items) {
-                    res.json(items);
-                }); 
-            });
-        });
-    //Search query
-    } else {
-        console.log('hits search');
-        console.log('uni-query ' +req.query.uni);
-        ddAPI.adapter.find('university', req.query.uni).then(function(uni) {
-            console.log('uni found');
-            findMenuItems(uni.universities[0].links.restaurants, req.query.search, function(items) {
-                console.log('makes it to callback');
-                res.json(items);
-            });
-        });
-    }
-});
-*/
 
 app.get('/', function(req, res) {
     res.send('Hello, you have reached the API.');
